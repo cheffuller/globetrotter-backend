29fb274b02ab074124687dd38b29630ad7c846e4
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-23 15:32:37 -0500

    Added FollowRepository. Requires unit testing.

--- src/main/java/com/revature/globetrotters/repository/FollowRepository.java
+++ src/main/java/com/revature/globetrotters/repository/FollowRepository.java
@@ -3,10 +3,14 @@ package com.revature.globetrotters.repository;
 import com.revature.globetrotters.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
-    @Query("")
-    List<Follow> findFollowerAndFollowingByFollower(int follower);
+    @Query("SELECT f.follower, f.followwing FROM Follow f WHERE f.follower = :follower")
+    List<Follow> findFollowerAndFollowingByFollower(@Param("follower") int follower);
+
+    @Query("SELECT f.follower, f.followwing FROM Follow f WHERE f.following = :following")
+    List<Follow> findFollowerAndFollowingByFollowing(@Param("following") int following);
 }
