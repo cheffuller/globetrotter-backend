ff812f11c9f19a83688ebe05a103a039c3a51a1d
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-23 15:26:53 -0500

    Added missing entity class from main.

--- src/main/java/com/revature/globetrotters/repository/FollowRepository.java
+++ src/main/java/com/revature/globetrotters/repository/FollowRepository.java
@@ -1,4 +1,12 @@
 package com.revature.globetrotters.repository;
 
-public class FollowRepository {
+import com.revature.globetrotters.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface FollowRepository extends JpaRepository<Follow, Integer> {
+    @Query("")
+    List<Follow> findFollowerAndFollowingByFollower(int follower);
 }
