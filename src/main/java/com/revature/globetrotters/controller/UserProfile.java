658160e6788d77ae7956652d759ec34e3c61bc42
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-23 14:48:52 -0500

    Added UserProfile entity.

---
+++ src/main/java/com/revature/globetrotters/controller/UserProfile.java
@@ -0,0 +1,62 @@
+package com.revature.globetrotters.controller;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+    @Column(name = "account_id")
+    @Id
+    private Integer accountId;
+    @Column(name = "bio")
+    private String bio;
+    @Column(name = "display_name")
+    private String displayName;
+    @Column(name = "is_private")
+    private boolean isPrivate;
+
+    public UserProfile() {
+    }
+
+    public UserProfile(Integer accountId, String bio, String displayName, boolean isPrivate) {
+        this.accountId = accountId;
+        this.bio = bio;
+        this.displayName = displayName;
+        this.isPrivate = isPrivate;
+    }
+
+    public Integer getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+}
