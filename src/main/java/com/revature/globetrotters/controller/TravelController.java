0787bbab855b0ab8f7d7213d0b0c0c78aad2b5e3
bhnguyen1 <nguyen.brandon316@gmail.com>
2024-12-24 12:17:38 -0600

    draft for patch

--- src/main/java/com/revature/globetrotters/controller/TravelController.java
+++ src/main/java/com/revature/globetrotters/controller/TravelController.java
@@ -4,9 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.revature.globetrotters.entity.TravelPlan;
+import com.revature.globetrotters.entity.TravelPlanLocation;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
 @Controller
 @RequestMapping("/accounts")
 @CrossOrigin(origins = "http://localhost:5173/")
 public class TravelController {
-    
+    @PatchMapping("/plans/{id}")
+    public ResponseEntity<?> updateTravelTimes(@PathVariable TravelPlanLocation travelPlan) {
+        try {
+            // call service layer to update travel plan by 
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
