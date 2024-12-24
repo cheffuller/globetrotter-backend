ced263925dce37dccb30cc8e91a74a1800825ac1
bhnguyen1 <nguyen.brandon316@gmail.com>
2024-12-24 14:37:58 -0600

    Finished service table

--- src/main/java/com/revature/globetrotters/repository/TravelPlanLocationRepository.java
+++ src/main/java/com/revature/globetrotters/repository/TravelPlanLocationRepository.java
@@ -11,15 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TravelPlanLocationRepository extends JpaRepository<TravelPlanLocation, Integer> {
-    @Query("SELECT t.city, t.country, t.endDate, t.startDate FROM TravelPlanLocation t WHERE t.travelPlanId = :travelPlanId")
+    @Query("SELECT tpl FROM TravelPlanLocation tpl WHERE tpl.travelPlanId = :travelPlanId")
     List<TravelPlanLocation> findLocationsByTravelPlanId(@Param("travelPlanId") int travelPlanId);
-
-    @Query("SELECT t.city,t.country, t.endDate, t.startDate, t.travelPlanId FROM TravelPlanLocation t WHERE t.city = :city AND t.country = :country")
-    List<TravelPlanLocation> findLocationsByCityAndCountry(@Param("city") String city, @Param("country") String country);
-
-    @Query("SELECT t.startDate, t.endDate FROM TravelPlanLocation t WHERE t.city = :city AND t.country = :country")
-    List<TravelPlanLocation> findDatesByCityAndCountry(@Param("city") String city, @Param("country") String country);
-
-    @Query("SELECT t.startDate, t.endDate FROM TravelPlanLocation t WHERE t.travelPlanId = :travelPlanId")
-    List<TravelPlanLocation> findDatesByTravelPlanId(@Param("travelPlanId") int travelPlanId);
 }
