39e08d48797d58bb694e4a512a2cf250ab377e5a
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-27 11:56:46 -0500

    Added security config to permit all API endpoints without authentication during testing.

--- src/main/java/com/revature/globetrotters/security/SecurityConfig.java
+++ src/main/java/com/revature/globetrotters/security/SecurityConfig.java
@@ -2,10 +2,22 @@ package com.revature.globetrotters.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .anyRequest().permitAll()
+        );
+        return http.build();
+    }
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
