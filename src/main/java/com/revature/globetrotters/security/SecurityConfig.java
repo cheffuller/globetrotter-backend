1749f17bebf25f3209a16c5a02922a0477021b2c
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-27 12:07:52 -0500

    Added security config to permit all API endpoints without authentication during testing.

--- src/main/java/com/revature/globetrotters/security/SecurityConfig.java
+++ src/main/java/com/revature/globetrotters/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                 .anyRequest().permitAll()
-        );
+        ).csrf(csrf -> csrf.disable());
         return http.build();
     }
 
