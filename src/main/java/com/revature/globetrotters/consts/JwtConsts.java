971f571cafce9db87ed55ca05dd82ca4b6e2ba54
Lawrence Yim <lawrence.yim.010@gmail.com>
2024-12-30 07:21:57 -0500

    Enabled filter in the configs, requiring a JWT token for all API end points except login and register.

--- src/main/java/com/revature/globetrotters/consts/JwtConsts.java
+++ src/main/java/com/revature/globetrotters/consts/JwtConsts.java
@@ -2,5 +2,4 @@ package com.revature.globetrotters.consts;
 
 public class JwtConsts {
     public static final String AUTHORIZATION = "authorization";
-    public static final String USERNAME = "username";
 }
--- src/main/java/com/revature/globetrotters/security/SecurityConfig.java
+++ src/main/java/com/revature/globetrotters/security/SecurityConfig.java
@@ -6,20 +6,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().permitAll()
-        ).csrf(csrf -> csrf.disable());
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/users/login").permitAll();
+                    authorize.requestMatchers("/users/register").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
     }
 }
