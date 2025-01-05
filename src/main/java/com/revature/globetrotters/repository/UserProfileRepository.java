8de11d19c9e8b27064bfd6b4d9e80fbb8e71b0b0
cheffuller <cheffuller@gmail.com>
2025-01-05 11:56:56 -0600

    edits to restore functionality from merge conflict edits

--- src/main/java/com/revature/globetrotters/repository/UserProfileRepository.java
+++ src/main/java/com/revature/globetrotters/repository/UserProfileRepository.java
@@ -10,22 +10,31 @@ import java.util.Optional;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
-    
-    @Query("""
-            SELECT up.displayName
-            FROM UserProfile up
-            JOIN UserAccount ua
-            ON up.accountId = ua.id
-            WHERE ua.username = :username
-            """)
-    Optional<String> findDisplayNameFromUsername(@Param("username") String username);
 
-    @Query("""
-            SELECT up 
-            FROM UserProfile up
-            JOIN UserAccount ua
-            ON ua.id = up.accountId
-            WHERE ua.username = :username""")
-    Optional<UserProfile> findByUsername(@Param("username") String username);
+        @Query("""
+                        SELECT up.displayName
+                        FROM UserProfile up
+                        JOIN UserAccount ua
+                        ON up.accountId = ua.id
+                        WHERE ua.username = :username
+                        """)
+        Optional<String> findDisplayNameFromUsername(@Param("username") String username);
+
+        @Query("""
+                        SELECT up.displayName
+                        FROM UserProfile up
+                        JOIN UserAccount ua
+                        ON up.accountId = ua.id
+                        WHERE ua.id = :userId
+                        """)
+        Optional<String> findDisplayNameFromUserId(@Param("userId") Integer userId);
+
+        @Query("""
+                        SELECT up
+                        FROM UserProfile up
+                        JOIN UserAccount ua
+                        ON ua.id = up.accountId
+                        WHERE ua.username = :username""")
+        Optional<UserProfile> findByUsername(@Param("username") String username);
 
 }
